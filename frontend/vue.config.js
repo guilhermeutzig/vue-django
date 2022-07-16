d47310b03372721930fb451439021428c5c81a3a
Guilherme Utzig <guilherme@sudomafia.com>
2022-07-16 13:01:30 -0300

    Finished setting server up

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const BundleTracker = require("webpack-bundle-tracker");
 
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: "http://0.0.0.0:8080",
+  outputDir: "./dist/",
+  devServer: {
+    static: {
+      publicPath: "http://0.0.0.0:8080",
+    },
+    port: 8080,
+    https: false,
+    headers: {
+      "Access-Control-Allow-Origin": ["*"],
+    },
+  },
+  chainWebpack: (config) => {
+    config.optimization.splitChunks(false);
+    config.plugin("BundleTracker").use(BundleTracker, [
+      {
+        filename: "./webpack-stats.json",
+      },
+    ]);
+    config.resolve.alias.set("__STATIC__", "static");
+  },
+};
