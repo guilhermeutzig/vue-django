462f70a6bd2b966dbbcf9f0a08efae1ff6cd0fb6
Guilherme Utzig <guilherme@sudomafia.com>
2022-07-16 14:09:24 -0300

    Created initial models, migrations and API routes

--- todo/settings.py
+++ todo/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webpack_loader'
+    'todo',
+    'webpack_loader',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -120,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
