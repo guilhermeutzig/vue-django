558fa3cdf4fc025eba3f15910894dee1a209e20f
Guilherme Utzig <guilherme@sudomafia.com>
2022-07-21 14:21:50 -0300

    Deploy test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,7 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/static/dist/"
-      : "http://0.0.0.0:8080",
+  publicPath: "/static/dist/",
   outputDir: "../static/dist/",
   indexPath: "../../templates/base-vue.html",
   devServer: {
@@ -16,7 +13,6 @@ module.exports = {
     devMiddleware: {
       writeToDisk: (filePath) => filePath.endsWith("index.html"),
       headers: { "Acess-Control-Allow-Origin": "*" },
-      publicPath: "http://localhost:8080",
     },
   },
 };
